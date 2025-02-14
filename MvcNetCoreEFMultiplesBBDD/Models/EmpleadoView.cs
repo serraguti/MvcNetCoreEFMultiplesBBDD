b2f0981b42ee7c53ebb115f507986f42847fc33f
serraguti <pacoserranox@gmail.com>
2025-02-14 12:34:08 +0100

    Add project files.

---
+++ MvcNetCoreEFMultiplesBBDD/Models/EmpleadoView.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcNetCoreEFMultiplesBBDD.Models
+{
+    [Table("V_EMPLEADO")]
+    public class EmpleadoView
+    {
+        [Key]
+        [Column("IDEMPLEADO")]
+        public int IdEmpleado { get; set; }
+        [Column("APELLIDO")]
+        public string Apellido { get; set; }
+        [Column("OFICIO")]
+        public string Oficio { get; set; }
+        [Column("SALARIO")]
+        public int Salario { get; set; }
+        [Column("DEPARTAMENTO")]
+        public string Departamento { get; set; }
+        [Column("LOCALIDAD")]
+        public string Localidad { get; set; }
+    }
+}
---
+++ MvcNetCoreEFMultiplesBBDD/Repositories/RepositoryEmpleados.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcNetCoreEFMultiplesBBDD.Data;
+using MvcNetCoreEFMultiplesBBDD.Models;
+
+#region VISTAS Y PROCEDIMIENTOS
+/*
+ create view V_EMPLEADOS
+as
+	select EMP_NO as IDEMPLEADO
+	, EMP.APELLIDO, EMP.OFICIO, EMP.SALARIO
+	, DEPT.DNOMBRE AS DEPARTAMENTO
+	, DEPT.LOC AS LOCALIDAD
+	from EMP
+	inner join DEPT
+	on EMP.DEPT_NO = DEPT.DEPT_NO
+go
+ */
+#endregion
+
+namespace MvcNetCoreEFMultiplesBBDD.Repositories
+{
+    public class RepositoryEmpleados
+    {
+        private HospitalContext context;
+
+        public RepositoryEmpleados(HospitalContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<EmpleadoView>> GetEmpleadosAsync()
+        {
+            var consulta = from datos in this.context.EmpleadosView
+                           select datos;
+            return await consulta.ToListAsync();
+        }
+
+        public async Task<EmpleadoView> FindEmpleadoAsync(int idEmpleado)
+        {
+            var consulta = from datos in this.context.EmpleadosView
+                           where datos.IdEmpleado == idEmpleado
+                           select datos;
+            return await consulta.FirstOrDefaultAsync();
+        }
+    }
+}
